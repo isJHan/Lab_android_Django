14202dc7a9a36293f10f4dd3fa33eb8e88ae718c
JHan <1052480931@qq.com>
2021-05-16 00:15:55 +0800

    建完comment表

--- YikE/YikE/settings.py
+++ YikE/YikE/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'accounts.apps.AccountsConfig',
     'todayCard.apps.TodaycardConfig',
+    'comments.apps.CommentsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- YikE/comments/models.py
+++ YikE/comments/models.py
@@ -1,3 +1,9 @@
 from django.db import models
-
+from django.db.models.deletion import CASCADE
+from todayCard.models import TodayCard
+from accounts.models import user
 # Create your models here.
+class comment(models.Model):
+    tcid = models.ForeignKey(TodayCard, on_delete=CASCADE)
+    ctext = models.CharField(max_length=50)
+    uid = models.ForeignKey(user, on_delete=CASCADE)
