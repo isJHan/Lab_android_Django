e47f0d561d223b44166bd557c87ba325459d2536
JHan <1052480931@qq.com>
2021-05-16 22:57:44 +0800

    暂存, 计划做admin界面图片的inline显示

--- YikE/todayCard/admin.py
+++ YikE/todayCard/admin.py
@@ -1,7 +1,20 @@
 # from YikE.todayCard.models import QnjrCard, TodayCard
 from .models import TodayCard, QnjrCard
 from django.contrib import admin
-
+from django.utils.safestring import mark_safe
 # Register your models here.
 admin.site.register(TodayCard)
 admin.site.register(QnjrCard)
+
+# @admin.register(TodayCard)
+# class HeroAdmin(admin.ModelAdmin):
+
+#     readonly_fields = ['picture', "headshot_image"]
+
+#     def headshot_image(self, obj):
+#         return mark_safe('<img src="{url}" width="{width}" height={height} />'.format(
+#             url = obj.headshot.url,
+#             width=obj.headshot.width,
+#             height=obj.headshot.height,
+#             )
+#     )
