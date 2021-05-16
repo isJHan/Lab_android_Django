30ab7e84113f37bffdabaf87b4e7cddc03d5d324
JHan <1052480931@qq.com>
2021-05-17 00:35:51 +0800

    完成通过url直接返回图片(暴露本地静态资源), 使用Django自带管理程序, Django NB !

--- YikE/YikE/urls.py
+++ YikE/YikE/urls.py
@@ -14,12 +14,17 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+from django.views.static import serve
+from .settings import MEDIA_PATH
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/accounts/', include('accounts.urls')),
     path('api/todayCard/', include('todayCard.urls')),
     path('api/comments/', include('comments.urls')),
+    # path('api/media/', include('media.urls')),
     # path('admin/', admin.site.urls),
+    # 访问静态文件, url按顺序匹配, 下面这行优先级最低, 必须放到最后一行
+    re_path(r'^api/(?P<path>.*)$', serve, {"document_root":MEDIA_PATH}), # 疑问, 为什么可以匹配这个而不影响其他路由的匹配
 ]
