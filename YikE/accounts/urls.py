78ddad6bc89c20b091e26c2a39831fc727f9e408
JHan <1052480931@qq.com>
2021-05-17 21:36:06 +0800

    将api整理到了api.json中, 大概接近尾声

--- YikE/accounts/urls.py
+++ YikE/accounts/urls.py
@@ -1,5 +1,5 @@
 
-from django.urls import path
+from django.urls import path, re_path
 from . import views
 
 appname = 'accounts'
@@ -7,7 +7,8 @@ appname = 'accounts'
 urlpatterns = [
     path(r'register/', views.register), # 注册
     path(r'login/', views.login), # 登录
-    path(r'getUserInfo/', views.getUserInfo),
+    path(r'getMyInfo/', views.getMyInfo), # 获取我的信息
+    re_path(r'^getUserInfo/$', views.getUserInfo), #　获取uid用户信息
     # path(r'illegal/', views.illegal), # 需要登陆的操作
     # path(r'getToken/', views.getToken), # 获取Token, 以便使用Django的认证系统
     path(r'logout/', views.logout), # 退出登录
